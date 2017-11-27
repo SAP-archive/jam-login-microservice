9634220f77d796eaa8d1fdeeebd0cb3af187bf2f
Robert Hetzler <rob.hetzler@sap.com>
2017-11-27 11:32:12 -0800

    Update Jenkins build to support new containers, updated build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-WORKDIR=$(dirname $0)/..
-DOCKERDIR=$WORKDIR/docker
-
+WORKDIR=$(dirname $0)
+BASE_IMAGE=kora/login-proxy
 
 REGISTRY=clm-registry.mo.sap.corp:5000
 
-PROXY=http://proxy.wdf.sap.corp:8080
-NO_PROXY=github.wdf.sap.corp
-
 BUILD_ENVS=()
-DISABLE_PROXY=
+PROXY_ARG=
 
 while [ -n "$1" ]; do
     COMMAND="$1"
@@ -22,11 +18,8 @@ while [ -n "$1" ]; do
         --dev)
             BUILD_ENVS=( ${BUILD_ENVS[@]} dev )
             ;;
-        --test)
-            BUILD_ENVS=( ${BUILD_ENVS[@]} test )
-            ;;
         --disable-proxy)
-            DISABLE_PROXY=true
+            PROXY_ARG=" --disable-proxy "
             ;;
     esac
 done
@@ -37,33 +30,33 @@ if [ -z "${BUILD_ENVS[*]}" ]; then
 fi
 
 #
-# construct (or not) proxy configuration to pass into build
-#
-
-PROXY_PARAMS=( HTTPS_PROXY https_proxy HTTP_PROXY http_proxy )
-NO_PROXY_PARAMS=( NO_PROXY no_proxy )
-
-PROXY_ARGS=
-
-if [ -z $DISABLE_PROXY ]; then
-    for param_name in ${PROXY_PARAMS[@]}; do
-        PROXY_ARGS="${PROXY_ARGS} --build-arg $param_name=$PROXY"
-    done
-    for param_name in ${NO_PROXY_PARAMS[@]}; do
-        PROXY_ARGS="${PROXY_ARGS} --build-arg $param_name=$NO_PROXY"
-    done
-fi
-
-#
 # for each env we want to build, build it with a custom name
 # and whichever proxy parameterization we've chosen
 #
+REGISTRY_IMAGE=${REGISTRY}/${BASE_IMAGE}
+GIT_REF=$( git rev-parse --short --verify HEAD )
+TAG=$( whoami )-${GIT_REF}
 
 for BUILD_ENV in ${BUILD_ENVS[@]}; do
 
-    IMAGE_NAME=clm-loginproxy-${BUILD_ENV}
+    case $BUILD_ENV in
+      dev)
+        ${WORKDIR}/jenkins-build.sh dev \
+          ${REGISTRY_IMAGE}-dev:$TAG \
+          $PROXY_ARG \
+          --prompt ${BASE_IMAGE}-dev \
+          --build-info "local-dev-build-${TAG}-$( date '+%Y-%m-%d@%H.%M.%S' )"
 
-    docker build $PROXY_ARGS \
-        --build-arg MIX_ENV=$BUILD_ENV \
-        -t $REGISTRY/$IMAGE_NAME -f $DOCKERDIR/Dockerfile $WORKDIR
+        [ "$?" == "0" ] || { echo "Dev build failed"; exit 1; }
+        ;;
+      prod)
+        ${WORKDIR}/jenkins-build.sh prod \
+          ${REGISTRY_IMAGE}:$TAG \
+          --build ${REGISTRY_IMAGE}-build:$TAG \
+          $PROXY_ARG \
+          --prompt ${BASE_IMAGE}-prod \
+          --build-info "local-prod-build-${TAG}-$( date '+%Y-%m-%d@%H.%M.%S' )"
+        [ "$?" == "0" ] || { echo "Prod build failed"; exit 1; }
+        ;;
+    esac
 done
--- docker/jenkins-build.sh
+++ docker/jenkins-build.sh
@@ -14,13 +14,24 @@ DOCKERDIR=$WORKDIR/docker
 #
 # Params
 #
-MIX_ENV="$1"
+BUILD_TYPE="$1"
 IMAGE="$2"
 
 shift; shift;
 
-if [ -z "$MIX_ENV" ] || [ -z "$IMAGE" ]; then
-  echo "Usage: $0 MIX_ENV IMAGE_NAME:TAG [ --disable-proxy ] [ --prompt PS1_PREFIX ] [ --build-info INFO ]"
+if [ -z "$BUILD_TYPE" ] || [ -z "$IMAGE" ]; then
+  echo "Usage: $0 BUILD_TYPE IMAGE_NAME:TAG [ --build BUILD-IMG:TAG ] [ --disable-proxy ] [ --prompt PS1_PREFIX ] [ --build-info INFO ]"
+  echo
+  echo "BUILD_TYPE can be one of:"
+  echo "   dev"
+  echo "   prod"
+  echo "and will build Dockerfile.dev and Dockerfile.prod, respectively."
+  echo " (if the dockerfile does not exist, it will fallback to Dockerfile (no extension)"
+  echo
+  echo "Resulting image will be taged to IMAGE_NAME:TAG"
+  echo
+  echo "If --build BUILD-IMG:TAG is provided, the build image will be built referencing Dockerfile.build"
+  echo " before either prod or dev"
   echo
   echo "--prompt PS1_PREFIX adds a prefix to the PS1 prompt when shelling into the container\n"
   echo "                    (this is useful for indicating what container you are in)"
@@ -30,6 +41,7 @@ if [ -z "$MIX_ENV" ] || [ -z "$IMAGE" ]; then
 fi
 
 DISABLE_PROXY=
+BUILD=
 
 while [ -n "$1" ]; do
     COMMAND="$1"
@@ -46,9 +58,16 @@ while [ -n "$1" ]; do
             INFO="$1"
             shift
             ;;
+        --build)
+            BUILD="$1"
+            shift
+            ;;
     esac
 done
 
+#
+# All of the complex stuffs needed to make this work behind (or not) the proxy
+#
 
 PROXY_PARAMS=( HTTPS_PROXY https_proxy HTTP_PROXY http_proxy )
 NO_PROXY_PARAMS=( NO_PROXY no_proxy )
@@ -66,19 +85,56 @@ if [ -z $DISABLE_PROXY ]; then
     done
 fi
 
+#
+# Let our shell be excellent when we exec a container
+#
+
 if [ -n "$PROMPT" ]; then
   PROMPT_ARG="--build-arg PROMPT=$PROMPT"
 fi
+
+#
+# Bake build information right into the container
+#
 
 if [ -n "$INFO" ]; then
   INFO_ARG="--build-arg INFO=$INFO"
 fi
 
+#
+# Generate build image if specified
+#
+
+PREBUILD_ARG=
+
+if [ -n "$BUILD" ]; then
+  BUILDFILE="$DOCKERDIR/Dockerfile.build"
+  [ -f "$BUILDFILE" ] || { echo "No $BUILDFILE, bailing"; exit 1; }
+  docker build \
+    $PROXY_ARGS \
+    $PROMPT_ARG \
+    $INFO_ARG \
+    -t $BUILD \
+    -f "$BUILDFILE" \
+    $WORKDIR
+  [ "$?" == 0 ] || { echo "Build container failed"; exit 1; }
+  PREBUILD_ARG="--build-arg BUILD_IMAGE=$BUILD"
+fi
+
+DOCKERFILE=$DOCKERDIR/Dockerfile.$BUILD_TYPE
+
+if [ ! -f "$DOCKERFILE" ]; then
+  echo -n "$DOCKERFILE not found, fallling back to "
+  DOCKERFILE=$DOCKERDIR/Dockerfile
+  echo "$DOCKERFILE"
+fi
+[ -f "$DOCKERFILE" ] || { echo "Fallback $DOCKERFILE not found, exiting"; exit 1; }
+
 docker build \
   $PROXY_ARGS \
   $PROMPT_ARG \
   $INFO_ARG \
-  --build-arg MIX_ENV=$MIX_ENV \
+  $PREBUILD_ARG \
   -t $IMAGE \
-  -f $DOCKERDIR/Dockerfile \
+  -f "$DOCKERFILE" \
   $WORKDIR
